e8d362350676ed23fc07895ace4e41db310faedb
Dhruvesh Potekar <dhruveshpotekar@gmail.com>
2026-03-14 16:37:44 +0100

    Added walltime calculation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "Solver.h"
+#include <chrono>
+#include <iostream>
 
 int main() {
+
     int nx = 100;
     double x_start = 0.0;
     double x_end = 1.0;
@@ -11,7 +14,19 @@ int main() {
     Solver solver(nx, x_start, x_end, a, cfl, final_time);
 
     solver.initializeGaussian(0.3, 0.08);
+
+    // start timer
+    auto start = std::chrono::high_resolution_clock::now();
+
     solver.run();
+
+    // stop timer
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = end - start;
+
+    std::cout << "Wall time = " << elapsed.count() << " seconds\n";
+
     solver.writeSolutionWithExact("solution.dat");
 
     return 0;
